25e963b1df333ea3e02bf228422ad2d29aef5497
Khushi Goriya <khushigoriya@vebuin.com>
2025-04-29 15:15:37 +0530

    Revert "[additon] fastapi files"

--- fastapi_app/common/__init__.py
+++
@@ -1,7 +0,0 @@
-from .constants import *
-from .enums import *
-from .exception import *
-from .errors import *
-from .logger import *
-from .schema import *
-from .utils import *
--- fastapi_app/common/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-root = logging.getLogger()
-if root.handlers:
-    for handler in root.handlers:
-        root.removeHandler(handler)
-logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
